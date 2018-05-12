27d469cc2ef5a95e82984126e99c5d4a385fd390
Poh <yiishernpoh@gmail.com>
2018-05-12 13:33:25 +0800

    second commit

---
+++ app/src/main/java/com/example/coolweather/db/Country.java
@@ -0,0 +1,47 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Country extends DataSupport {
+
+    private int id;
+
+    private String countryName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+}
