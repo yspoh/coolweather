61d92442a1705141c7612b341bc5bf333bbbe82d
Poh <yiishernpoh@gmail.com>
2018-05-13 17:00:34 +0800

    third

--- app/src/main/java/com/example/coolweather/db/City.java
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -16,31 +16,32 @@ public class City extends DataSupport {
         return id;
     }
 
-    public String getCityName() {
-        return cityName;
-    }
-
-    public int getCityCode() {
-        return cityCode;
-    }
-
-    public int getProvinceId() {
-        return provinceId;
-    }
-
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
     }
 
     public void setCityName(String cityName) {
         this.cityName = cityName;
     }
 
+    public int getCityCode() {
+        return cityCode;
+    }
+
     public void setCityCode(int cityCode) {
         this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
     }
 
     public void setProvinceId(int provinceId) {
         this.provinceId = provinceId;
     }
+
 }
