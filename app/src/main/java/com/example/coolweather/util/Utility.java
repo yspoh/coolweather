6b6a7dc15a3c7cfbdd5921de1b1da68caf7ff5d1
Poh <yiishernpoh@gmail.com>
2018-05-13 23:45:27 +0800

    ok

--- app/src/main/java/com/example/coolweather/util/Utility.java
+++ app/src/main/java/com/example/coolweather/util/Utility.java
@@ -6,6 +6,9 @@ import com.example.coolweather.db.City;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.County;
 import com.example.coolweather.db.Province;
+import com.example.coolweather.gson.Weather;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -79,6 +82,22 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    public static Weather handleWeatherResponse(String response)
+    {
+        try
+        {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONArray jsonArray = jsonObject.getJSONArray("HeWeather");
+            String weatherContent = jsonArray.getJSONObject(0).toString();
+            return new Gson().fromJson(weatherContent, Weather.class);
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
